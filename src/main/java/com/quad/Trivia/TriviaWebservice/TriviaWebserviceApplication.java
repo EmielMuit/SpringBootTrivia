b920bcace10ffe281065a2ae2dcc1d480b853265
EMuit <emielmuit@hotmail.com>
2020-08-22 16:50:44 +0200

    [Trivia] Initial commit

---
+++ src/main/java/com/quad/Trivia/TriviaWebservice/TriviaWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.quad.Trivia.TriviaWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quad/Trivia/TriviaWebservice/TriviaWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quad.Trivia.TriviaWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
